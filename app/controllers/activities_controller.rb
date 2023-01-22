e56fa50ed5252b5c13512a8ce8ea91abf423c76c
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2023-01-22 08:02:52 +0000

    [Edit] アクティビティ通知画面のレイアウト編集

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,15 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = current_user.active_notifications.page(params[:page]).per(20)
+     @activities.where(checked: false).each do |activity|
+      activity.update(checked: true)
+    end
     @genres = Genre.all
   end
   
    def destroy_all
     #通知を全削除
     @activities = current_user.passive_notifications.destroy_all
-  	redirect_to activities_path
+  	redirect_to request.referer 
   end
 end
