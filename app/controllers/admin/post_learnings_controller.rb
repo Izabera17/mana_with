9151d2cab03aef801639a953ce4b8bccd90feb25
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2023-01-29 06:41:58 +0000

    deploy setting

--- app/controllers/admin/post_learnings_controller.rb
+++ app/controllers/admin/post_learnings_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PostLearningsController < ApplicationController
   before_action :authenticate_user!
-　before_action :if_not_admin
+　before_action :if_not_admin, only: [:index]
   before_action :set_post_learning, only: [:index]
   
   def index 
