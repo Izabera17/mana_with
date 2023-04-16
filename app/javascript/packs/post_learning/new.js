24ce6500bd5f401ae3746cf73021a0ddd3a8e926
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2023-04-16 02:28:49 +0000

    [Edit] 投稿フォームの文字数表示

--- app/javascript/packs/post_learning/new.js
+++ app/javascript/packs/post_learning/new.js
@@ -1,10 +1,32 @@
-const text = document.querySelector('#post_learning_learning_content');
+const text_name = document.querySelector('#post_learning_learning_name');
+const text_content = document.querySelector('#post_learning_learning_content');
+const text_real = document.querySelector('#post_learning_learning_real');
 const count = document.querySelector('#count');
 
-text.addEventListener('keyup', () => {
-  count.textCount = text.value.length;
+text_name.addEventListener('keyup', () => {
+  count.textContent = text_name.value.length;
   
-  if (text.value.length > 100) {
+  if (text_name.value.length > 50) {
+    count.classList.add('alert');
+  } else {
+    count.classList.remove('alert');
+  }
+});
+
+text_content.addEventListener('keyup', () => {
+  count.textContent = text_content.value.length;
+  
+  if (text_content.value.length > 80) {
+    count.classList.add('alert');
+  } else {
+    count.classList.remove('alert');
+  }
+});
+
+text_real.addEventListener('keyup', () => {
+  count.textContent = text_real.value.length;
+  
+  if (text_real.value.length > 100) {
     count.classList.add('alert');
   } else {
     count.classList.remove('alert');
