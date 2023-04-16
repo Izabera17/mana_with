51062ce213269b63fcabadd5ce99cb02d4faa58d
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2023-04-16 02:34:07 +0000

    [Edit] 投稿フォームの文字数表示

--- app/javascript/packs/post_learning/new.js
+++ app/javascript/packs/post_learning/new.js
@@ -1,34 +1,36 @@
 const text_name = document.querySelector('#post_learning_learning_name');
 const text_content = document.querySelector('#post_learning_learning_content');
 const text_real = document.querySelector('#post_learning_learning_real');
-const count = document.querySelector('#count');
+const count_name = document.querySelector('#count_name');
+const count_content = document.querySelector('#count_content');
+const count_real = document.querySelector('#count_real');
 
 text_name.addEventListener('keyup', () => {
-  count.textContent = text_name.value.length;
+  count_name.textContent = text_name.value.length;
   
   if (text_name.value.length > 50) {
-    count.classList.add('alert');
+    count_name.classList.add('alert');
   } else {
-    count.classList.remove('alert');
+    count_name.classList.remove('alert');
   }
 });
 
 text_content.addEventListener('keyup', () => {
-  count.textContent = text_content.value.length;
+  count_content.textContent = text_content.value.length;
   
   if (text_content.value.length > 80) {
-    count.classList.add('alert');
+    count_content.classList.add('alert');
   } else {
-    count.classList.remove('alert');
+    count_content.classList.remove('alert');
   }
 });
 
 text_real.addEventListener('keyup', () => {
-  count.textContent = text_real.value.length;
+  count_real.textContent = text_real.value.length;
   
   if (text_real.value.length > 100) {
-    count.classList.add('alert');
+    count_real.classList.add('alert');
   } else {
-    count.classList.remove('alert');
+    count_real.classList.remove('alert');
   }
 });
